d1438d7eef33487cb917ea7881a9c824d013a196
Tech Zoro Tamil <atxbotz9@gmail.com>
2025-11-19 16:59:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
+# ───────────────────────────────────────────
+#   ⚙️ INSTALL & START (TECH ZORO TAMIL)
+#   Setup the bot and launch the service
+# ───────────────────────────────────────────
 pip install -r requirements.txt --no-cache-dir
 python3 main.py
